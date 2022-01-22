f01ddbf9aba29920734af1d3071f7c51b5e3f335
NeoTheOne90 <88240177+NEOTheOne90@users.noreply.github.com>
2022-01-22 15:54:18 +0900

    Numeric

---
+++ operator.js
@@ -0,0 +1,25 @@
+// 1. String concatenation
+console.log('my' + 'car'); //문자와 문자를
+console.log('1' + 2); //문자열에 숫자를 더하면 숫자가 =>문자열로 변환됨.
+console.log(`string literals:  1 + 2 = ${1 + 2}`); //백틱기호로 변수값을 계산해 스트링에 포함해서 출력됨
+
+console.log(`string literals:  
+
+''''' '속에 내용'
+1 + 2 = ${1 + 2}`); //중간에 다른 내용이 들어가도 출력이됨
+
+console.log("ellie\'s \n \tbook");
+// 중간에 특수문자 표시를 하고싶으면 앞에 \(백슬러시) 붙여줌.
+// 줄바꿈을 원하면 \n , 탭키처럼 띄우고 싶으면 \t를 사용한다.
+// 다양한 특수문자열이 있음.
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// 2. Numeric operators 숫자연산자
+console.log(1 + 1); // add 더하기
+console.log(1 - 1); // Subtract 빼기
+console.log(1 / 1); // divide 나누기
+console.log(1 * 1); // multiply 곱하기
+console.log(5 % 2); // remainder 나머지
+console.log(2 ** 3); // exponentiation 거듭제곱
