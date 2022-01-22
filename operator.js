a22ce9060ad21b3ac044b1e70069f1223457d87a
NeoTheOne90 <88240177+NEOTheOne90@users.noreply.github.com>
2022-01-22 16:11:40 +0900

    Increment and decrement

--- operator.js
+++ operator.js
@@ -17,9 +17,36 @@ console.log("ellie\'s \n \tbook");
 
 
 // 2. Numeric operators 숫자연산자
+
 console.log(1 + 1); // add 더하기
 console.log(1 - 1); // Subtract 빼기
 console.log(1 / 1); // divide 나누기
 console.log(1 * 1); // multiply 곱하기
 console.log(5 % 2); // remainder 나머지
-console.log(2 ** 3); // exponentiation 거듭제곱
+console.log(2 ** 3); // exponentiation 거듭제곱 2의3승
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//3. Increment and decrement operators 증가 , 감소연산자
+
+let counter = 2; 
+const preIncrement = ++counter;  //변수앞에 ++붙이면
+// counter = counter + 1;  카운터에 1을 더해서 다시 앞쪽 카운터에 숫자를 할당한다음
+// preIncrement = counter; 숫자 3을 preIncrement변수에 할당 = 3
+console.log(`preIncrement: ${preIncrement}, counter: ${counter}`);
+
+const postIncrement = counter++; //변수뒤쪽에 ++을붙이면
+// postIncrement = counter; 변수의 값을 먼저 증가시켜 할당함.
+// counter = counter + 1;   3을 할당한다음 counter값을 1증가 = 4
+console.log(`postIncrement: ${postIncrement}, counter: ${counter}`); 
+
+// 마이너스도 똑같다. 
+const preDecrement = --counter;   // 3
+console.log(`preDecrement: ${preDecrement}, counter: ${counter}`);
+
+const postDecrement = counter--;  // 2
+console.log(`postDecrement: ${postDecrement}, counter: ${counter}`); 
+
+///////////////////////////////////////////////////////////////////////////////////////////////
