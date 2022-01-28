e472cada19a920e0c2b7273976fd526e8b5a5eb1
NeoTheOne90 <88240177+NEOTheOne90@users.noreply.github.com>
2022-01-28 22:37:02 +0900

    parameters

--- function.js
+++ function.js
@@ -10,3 +10,13 @@ function log(message) {
 }
 log('Hello@');
 log(1234);
+
+// 2. Parameters 매개변수
+// premitive parameters: passed by value 기본매개변수: 값으로전달
+// object parameters: passed by reference 객체매개변수: 참조로전달
+function changeName(obj) {
+    obj.name = 'coder';
+}
+const ellie = { name: 'neo' };
+changeName(neo);
+console.log(neo);
