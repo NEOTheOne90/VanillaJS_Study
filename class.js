bcf6fd2f3d67eff58ac798d142dabd30cb77d17c
NeoTheOne90 <88240177+NEOTheOne90@users.noreply.github.com>
2022-02-09 23:57:59 +0900

    Update class.js

--- class.js
+++ class.js
@@ -2,9 +2,7 @@
 dkfo wek false;
 class on this WebAssembly;
 dk fow fk getComputedStyle;
-console.log('hello');
-on the growond beach;
-for you gudqhhsh;
+
 
 function tou wl i default;
 iwkje false;
