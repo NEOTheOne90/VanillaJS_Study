7a29964343dd692b914b4e816ca87ec3190a271b
NeoTheOne90 <88240177+NEOTheOne90@users.noreply.github.com>
2022-02-13 22:14:44 +0900

    class.3

--- class.js
+++ class.js
@@ -10,3 +10,8 @@ console.log(stringFive !== numberFive); // true
 
 const neo1 = { name: 'neo' };
 const neo2 = { name: 'neo' };
+
+const neo3 = neo1;  // neo1이 가지고있는 reference velue를 neo3에게 할당했기때문에 같아짐
+console.log(neo1 == neo2); //각각 다른 레퍼런스  ----- false
+console.log(neo1 === neo2);// 레퍼런스 값이 다르다. --- false
+console.log(neo1 === neo3); // reference velue값 할당  --- true
