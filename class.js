4621572d1d570653e2c9057d76533c7c378d2d4c
NeoTheOne90 <88240177+NEOTheOne90@users.noreply.github.com>
2022-02-13 22:13:58 +0900

    Update class.js

--- class.js
+++ class.js
@@ -3,3 +3,7 @@ so import co k wef;
 kld aim kl WebGLTransformFeedback;
 looki Animation;
 console.log('hello',);
+super animal royal
+bettle ground;
+console.log(stringFive === numberFive); // false
+console.log(stringFive !== numberFive); // true
