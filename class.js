f38905fbaa2889591fb4231fbed6f4cb09c8c643
NeoTheOne90 <88240177+NEOTheOne90@users.noreply.github.com>
2022-02-13 22:15:02 +0900

    class.4

--- class.js
+++ class.js
@@ -1,10 +1,5 @@
 'use strict';
-so import co k wef;
-kld aim kl WebGLTransformFeedback;
-looki Animation;
-console.log('hello',);
-super animal royal
-bettle ground;
+
 console.log(stringFive === numberFive); // false
 console.log(stringFive !== numberFive); // true
 
