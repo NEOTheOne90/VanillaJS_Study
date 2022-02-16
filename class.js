ff84d30b6956756c00f74e34572f9fcb0f039c32
NeoTheOne90 <88240177+NEOTheOne90@users.noreply.github.com>
2022-02-16 18:41:14 +0900

    update

--- class.js
+++ class.js
@@ -10,3 +10,9 @@ const neo3 = neo1;  // neo1이 가지고있는 reference velue를 neo3에게 할
 console.log(neo1 == neo2); //각각 다른 레퍼런스  ----- false
 console.log(neo1 === neo2);// 레퍼런스 값이 다르다. --- false
 console.log(neo1 === neo3); // reference velue값 할당  --- true
+
+const simplePrint = () => console.log('simplePrint!');
+const add = (a,b) => a + b; // 간결하게 표현이 가능하다.
+const simpleMultiply = (a, b) => {
+    return a * b;
+};
