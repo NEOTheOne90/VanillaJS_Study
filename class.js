4791b37b3db0efd9d7e43b1a98ee4e81652f1010
NeoTheOne90 <88240177+NEOTheOne90@users.noreply.github.com>
2022-02-18 23:20:01 +0900

    Update class.js

--- class.js
+++ class.js
@@ -1,9 +1,5 @@
 'use strict';
 
-const neo3 = neo1;  // neo1이 가지고있는 reference velue를 neo3에게 할당했기때문에 같아짐
-console.log(neo1 == neo2); //각각 다른 레퍼런스  ----- false
-console.log(neo1 === neo2);// 레퍼런스 값이 다르다. --- false
-console.log(neo1 === neo3); // reference velue값 할당  --- true
 
 console.log("ellie\'s \n \tbook");
 // 중간에 특수문자 표시를 하고싶으면 앞에 \(백슬러시) 붙여줌.
