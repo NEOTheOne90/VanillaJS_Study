d3548d802e85d0438d37ed4ef6e87ec3e1d2c9e4
NeoTheOne90 <88240177+NEOTheOne90@users.noreply.github.com>
2022-02-18 23:18:52 +0900

    Update class.js

--- class.js
+++ class.js
@@ -5,3 +5,7 @@ console.log(neo1 == neo2); //각각 다른 레퍼런스  ----- false
 console.log(neo1 === neo2);// 레퍼런스 값이 다르다. --- false
 console.log(neo1 === neo3); // reference velue값 할당  --- true
 
+console.log("ellie\'s \n \tbook");
+// 중간에 특수문자 표시를 하고싶으면 앞에 \(백슬러시) 붙여줌.
+// 줄바꿈을 원하면 \n , 탭키처럼 띄우고 싶으면 \t를 사용한다.
+// 다양한 특수문자열이 있음.
