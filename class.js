128b03cf117d67c1fac2bb6226a43b362a320b22
NeoTheOne90 <88240177+NEOTheOne90@users.noreply.github.com>
2022-02-22 21:20:37 +0900

    Update class.js

--- class.js
+++ class.js
@@ -1,10 +1,7 @@
 'use strict';
 
 
-console.log("ellie\'s \n \tbook");
-// 중간에 특수문자 표시를 하고싶으면 앞에 \(백슬러시) 붙여줌.
-// 줄바꿈을 원하면 \n , 탭키처럼 띄우고 싶으면 \t를 사용한다.
-// 다양한 특수문자열이 있음.
+
 
 let globalName = 'global name';
 {
